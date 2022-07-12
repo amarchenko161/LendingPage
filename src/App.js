21dad82b4086765d557ab349e2c49911322262aa
a.marchenko <amarchenko.exceedteam@gmail.com>
2022-07-12 11:34:39 +0300

    add companies component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Description from './Components/DescriptionComponent/Description';
 import Header from './Components/HeaderComponent/Header';
+import TrustedCompanies from './Components/TrustedCompaniesComponent/TrustedCompanies';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Description />
+      <TrustedCompanies />
     </div>
   );
 }
